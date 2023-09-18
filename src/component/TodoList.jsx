f5aad349717c8c4271d84eae939639b0455cd603
smr1004 <138188798+smr1004@users.noreply.github.com>
2023-09-18 14:11:50 +0900

    .

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -44,7 +44,7 @@ function TodoList() {
         <div className="analyzeTodo ">
           <div>총 할 일 개수 : {totalCount} ,</div>
           <div>
-            완료된 일 : {doneCount} , 미완료 할 일 : {dontCount} ,
+            완성된 일 : {doneCount} , 미완성 할 일 : {dontCount} ,
           </div>
         </div>
         <input
